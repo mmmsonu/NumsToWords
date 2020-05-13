da9d9b0be64b88fb176e76e17ec86e36e0e19212
manmohan <mmmsonu@gmail.com>
2020-05-13 20:04:04 +0530

    Add project files.

---
+++ NumsToWords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumsToWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            dynamic inputs = Console.ReadLine();
+            Console.WriteLine(inputs.ToString());
+        }
+    }
+}
+
+
+public class NumToWord
+{
+
+}
